1492d1a7ee929db65480de544c3f70852f4b4640
MayEscobar <mayraescobarsantander@gmail.com>
2026-05-29 00:45:31 +0200

    arreglo errores en visualizar archivo

--- Proyecto_IS_Mas-ISO29110/controllers/archivos/visualizar_controller.py
+++ Proyecto_IS_Mas-ISO29110/controllers/archivos/visualizar_controller.py
@@ -2,11 +2,12 @@ from flask import Blueprint, render_template, redirect, url_for, session, flash
 from alchemyClasses.archivo import obtener_por_curso, obtener_por_id
 from alchemyClasses.curso import obtener_por_id as obtener_curso_por_id
 
+
 visualizarArchivo_bp = Blueprint('visualizar_archivo', __name__)
 
 @visualizarArchivo_bp.route('/cursos/<int:id_curso>/archivos')
 def visualizar_archivos(id_curso):
-    if 'user_id' not in session:
+    if 'id_usuario' not in session:
         return redirect(url_for('auth.login'))
 
     curso_actual = obtener_curso_por_id(id_curso)
@@ -26,12 +27,22 @@ def visualizar_archivos(id_curso):
 
 @visualizarArchivo_bp.route('/cursos/<int:id_curso>/archivos/<int:id_archivo>')
 def detalle_archivo(id_curso, id_archivo):
-    if 'user_id' not in session:
+    if 'id_usuario' not in session:
         return redirect(url_for('auth.login'))
 
+    curso_actual = obtener_por_curso_por_id(id_curso)
+
+    if not curso_actual:
+        flash('Curso no existe', 'error')
+        return redirec(url_for('course.visualizar_cursos'))
+
     archivo = obtener_por_id(id_archivo)
-    curso_actual = obtener_curso_por_id(id_curso)
+    if not archivo:
+        flash('Archivo no existe', 'error')
+        return redirect(url_for('visualizar_archivo.visualizar_archivos', id_curso=id_curso))
+
     rol = session.get('rol')
+    
 
     return render_template(
         'detalle_archivo.html',
